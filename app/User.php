df91b3421b5d47249c0b03c26adf333652a9e14d
stoyantodorovbg <stoyan_11@abv.bg>
2018-11-06 09:01:42 +0200

    Add user profile image - Controller and Tests

--- app/User.php
+++ app/User.php
@@ -19,7 +19,10 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'avatar_path',
     ];
 
     /**
---
+++ tests/Feature/AddAvatarTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+
+class AddAvatarTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function only_members_can_add_avatars()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $this->json('POST', 'api/users/1/avatar');
+            //->assertStatus(401);
+    }
+
+    /** @test */
+    public function a_valid_avatar_must_be_provided()
+    {
+        $this->signIn();
+
+        $this->expectException('Illuminate\Validation\ValidationException');
+
+        $this->json('POST', 'api/users/'. auth()->id() .'/avatar', [
+            'avatar' => 'not_valid_image'
+        ]);
+    }
+
+    /** @test */
+    public function a_user_may_add_an_avatar_to_their_profile()
+    {
+        $this->signIn();
+
+        $file = UploadedFile::fake()->image('avatar.jpg');
+
+        Storage::fake('public');
+
+        $this->json('POST', 'api/users/'. auth()->id() .'/avatar', [
+            'avatar' => $file
+        ]);
+
+        $this->assertEquals('avatars/' . $file->hashName(), auth()->user()->avatar_path);
+
+        Storage::disk('public')->assertExists('avatars/' . $file->hashName());
+    }
+}
