39ae10dc6f362e14cebd2551ebcfa599566fa956
stoyantodorovbg <stoyan_11@abv.bg>
2018-12-08 20:52:35 +0200

    Languages - Model, migration, seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(ReplyTableSeeder::class);
         $this->call(ChannelTableSeeder::class);
+        $this->call(LanguagesTableSeeder::class);
+
     }
 }
---
+++ database/seeds/LanguagesTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class LanguagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('languages')->insert([
+            'title' => 'English',
+            'short_title' => 'en',
+            'status' => 1,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('languages')->insert([
+            'title' => 'Bulgarian',
+            'short_title' => 'bg',
+            'status' => 1,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
