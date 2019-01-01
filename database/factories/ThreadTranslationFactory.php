29192f83d42b34eff9ca9de1b93b5178ce75fd1b
stoyantodorovbg <stoyan_11@abv.bg>
2019-01-01 22:56:19 +0200

    Generate ThreadTranslation models with incremented thrad_id

--- database/factories/ThreadTranslationFactory.php
+++ database/factories/ThreadTranslationFactory.php
@@ -1,13 +1,23 @@
 <?php
 use Faker\Factory as Faker;
 
-$factory->define(\App\Models\ThreadTranslation::class, function () {
+$autoIncrement = autoIncrement();
+
+$factory->define(\App\Models\ThreadTranslation::class, function () use ($autoIncrement) {
     $faker = Faker::create('es_ES');
+    $autoIncrement->next();
 
     return [
-        'thread_id' => 1,
+        'thread_id' => $autoIncrement->current(),
         'language_id' => 3,
         'title' => $faker->realText(),
         'body' => $faker->realText(),
     ];
 });
+
+function autoIncrement()
+{
+    for ($i = 0; $i < 10; $i++) {
+        yield $i;
+    }
+}
